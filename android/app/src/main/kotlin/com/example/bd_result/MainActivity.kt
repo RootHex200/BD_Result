49a342a044f34fd9cc8ae3d1dcadf7e11c3113fc
roothex200 <sabiturrahman60@gmail.com>
2023-09-10 10:32:09 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/bd_result/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bd_result
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+
+import 'package:bd_result/src/view/home/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp( const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BD Result',
+      debugShowCheckedModeBanner: false,
+      
+      theme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+
+      home:const Homepage(),
+       builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.3),
+          child: child!,
+        );
+      }
+    );
+  }
+}
+
